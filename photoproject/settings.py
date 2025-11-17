fb028f9ce1e0b5a31690b0f5fa392221149e6460
tkyzar52 <tky2502052@stu.o-hara.ac.jp>
2025-11-17 12:41:42 +0900

    Update settings.py

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6avxnxkd+7s$q+e$nr!viodg2quhplr2r323l9(6wzx*(#c9e-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['photosystem4taka.pythonanywhere.com']
+ALLOWED_HOSTS = ['photosystem4zar.pythonanywhere.com']
 
 
 # Application definition
