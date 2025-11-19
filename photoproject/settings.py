71c01721204562e5897e87ddb3de81fc7b8b5618
tkyzar52 <tky2502052@stu.o-hara.ac.jp>
2025-11-19 09:36:28 +0900

    Update settings.py

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6avxnxkd+7s$q+e$nr!viodg2quhplr2r323l9(6wzx*(#c9e-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['photosystem4zar.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 AUTH_USER_MODEL = 'accounts.CustomUser'
